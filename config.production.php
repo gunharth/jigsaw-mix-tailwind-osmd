618f5765d36676deb75d54ea3bcea3acf8207a65
Gunharth Randolf <hello@gunharth.io>
2018-01-11 17:52:52 +0100

    Change destination folder for production to use github docs folder

---
+++ config.production.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'baseUrl' => 'http://gunharth.io/jigsaw-mix-tailwind-osmd',
+    'production' => true,
+    'build' => [
+        'destination' => 'docs',
+    ],
+];
--- source/index.blade.php
+++ source/index.blade.php
@@ -5,7 +5,7 @@
 <ul class="list-reset leading-tight">
 @foreach ($posts as $post)
     <li class="font-sans py-4">
-        <a class="post-title" href="{{ $post->getPath() }}">{{ $post->title }}</a></li>
+        <a class="post-title" href="{{ $page->baseUrl }}{{ $post->getPath() }}">{{ $post->title }}</a></li>
         <div class="text-sm text-grey mb-3">
             <span class="font-bold">Published on</span>
             {{ $post->dateFormatted() }}
